ee6d58717e77056047676b91a99ca4589bc7ec56
MagicShawn <1056472710@qq.com>
2025-03-29 18:00:04 +0800

    update slam algorithm params!

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,12 @@
+# 启动机器人人仿真
 roslaunch robot_description gazebo.launch
+
 roslaunch robot_description controller.launch
 
-roslaunch fast
+# 启动fastlio
+roslaunch fast_lio mapping_velodyne.launch
+# 启动fastlio_sam
+roslaunch fast_lio_sam mapping_velodyne16_lio_sam_parking_dataset.launch
 
 # move control
 roslaunch robot_control velocity_controller.launch 
@@ -9,6 +14,7 @@ roslaunch robot_control velocity_controller.launch
 rosrun teleop_twist_keyboard teleop_twist_keyboard.py
 
 # 启动算法仿真RViz
-rosrun rviz rviz -d /home/graduation/Work_space/Photovoltaic_robot_simulator/src/slam_algorithm/FAST_LIO/rviz_cfg/loam_livox.rviz
+rosrun rviz rviz -d $(rospack find fast_lio_sam)/rviz_cfg/loam_livox.rviz
+rosrun rviz rviz -d $(rospack find fast_lio)/rviz_cfg/loam_livox.rviz
 # 在独立命名空间中启动Gazebo仿真RViz
-ROS_NAMESPACE=gazebo_rviz rosrun rviz rviz -d /home/graduation/Work_space/Photovoltaic_robot_simulator/src/robot_simulator/robot_description/launch/urdf.rviz
+ROS_NAMESPACE=gazebo_rviz rosrun rviz rviz -d $(rospack find robot_description)/launch/urdf.rviz
